2f081d137f6c3324d094a9af6d2d97a4cbc77ff2
Zahar A. Ponimash <ponimashz@mail.ru>
2023-12-23 07:53:08 +0300

    Работа со сложными объектами

---
+++ EnvLib/ComplexObjectControl/Base/ObjectInversModel.cs
@@ -0,0 +1,48 @@
+﻿using AI.DataStructs.Algebraic;
+using AI.ML.Regression;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvLib.ComplexObjectControl.Base
+{
+    /// <summary>
+    /// Модель обратного процесса, к процессу управления
+    /// </summary>
+    [Serializable]
+    public class ObjectInversModel
+    {
+
+        public IMultyRegression<Vector> MultyRegression;
+
+        public ObjectInversModel() { }
+
+        public virtual Vector GetControl(Vector state) 
+        {
+            return MultyRegression.Predict(state);
+        }
+
+        public virtual void Train(ObjModelDataset dataset) 
+        {
+            MultyRegression.Train(dataset.States.ToArray(), dataset.ControlActions.ToArray());
+        }
+
+    }
+
+    [Serializable]
+    public class ObjModelDataset 
+    {
+        public List<Vector> States { get; set; } = new List<Vector>();
+
+        public List<Vector> ControlActions { get; set; } = new List<Vector>();
+
+
+        public void Add(Vector state, Vector action) 
+        {
+            States.Add(state);
+            ControlActions.Add(action);
+        }
+    }
+}
---
+++ Tests/MultyRegressionTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using AI.DataStructs.Algebraic;
+using AI.ML.NeuralNetwork.CoreNNW.Activations;
+using AI.ML.Regression;
+
+NeuralMultyRegression neuralMultyRegression = new NeuralMultyRegression(2, 3, 12, new TanhUnit());
+neuralMultyRegression.EpochesToPass = 50;
+
+Vector[] inps = { new Vector(1, 2), new Vector(2, 2), new Vector(2, 2), new Vector(1, -2) };
+Vector[] outps = { new Vector(1, 3, -1), new Vector(-2, 0, 2), new Vector(-2, 0, 2), new Vector(-1, -2, 1) };
+int k = 0;
+
+
+neuralMultyRegression.Train(inps, outps);
+
+var result = neuralMultyRegression.Predict(inps[k]);
+
+Console.WriteLine($"\n\n\nInp: {inps[k]}\n\nPredict: {result}");
